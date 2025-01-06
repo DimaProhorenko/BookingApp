d4c94dc6c4355020dab5b7670f6eea420daf42da
DimaProkhorenko <dima.study.web@gmail.com>
2025-01-06 12:03:27 +0100

    ADD server connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+import connectDB from "./db/connectDB.js";
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use("/", (req, res) => {
   return res.json({ msg: "Success" });
 });
 
-app.listen("5000", () => {
-  console.log("Server");
+mongoose.connection.once("open", () => {
+  app.listen(PORT, () => {
+    console.log("Server");
+  });
+});
+
+mongoose.connection.on("error", (e) => {
+  console.log(e);
 });
